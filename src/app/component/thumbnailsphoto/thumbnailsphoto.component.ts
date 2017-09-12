d7f4c0ea0c333fa58968abb4ba112d4bd890c518
Expert IT <expert.developer.it@gmail.com>
2017-09-12 10:36:14 +0530

    Grid Views

--- src/app/component/thumbnailsphoto/thumbnailsphoto.component.ts
+++ src/app/component/thumbnailsphoto/thumbnailsphoto.component.ts
@@ -1,17 +1,46 @@
 import { Component, OnInit, Input} from '@angular/core';
+import {ThumbnailsphotoService} from './thumbnailsphoto.service';
 
 @Component({
   selector: 'app-thumbnailsphoto',
   templateUrl: './thumbnailsphoto.component.html',
-  styleUrls: ['./thumbnailsphoto.component.css']
+  styleUrls: ['./thumbnailsphoto.component.css'],
+  providers: [ThumbnailsphotoService]
 })
 export class ThumbnailsphotoComponent implements OnInit {
 
   @Input() title:string="View Thumbnails Photo Points"; //--- ===create input for title 
 
-  constructor() { }
+  selectsecondoption:boolean;
+  selectfirstoption:boolean;
+  selectthirdoption:boolean;
+
+  constructor(private _sphotoService : ThumbnailsphotoService) { 
+  	this.selectfirstoption =true;
+  	this.selectsecondoption =false;
+  	this.selectthirdoption =false
+  }
 
   ngOnInit() {
+  }
+
+ // ---=== View select---====
+ firstoption(){
+ 	this.selectfirstoption=true;
+ 	this.selectsecondoption =false;
+  	this.selectthirdoption =false;
+
+ }
+
+  secondoption(){
+  	this.selectsecondoption =true;
+  	this.selectfirstoption =false;
+  	this.selectthirdoption =false;
+  }
+  thirdoption(){
+  	this.selectthirdoption =true;
+  	this.selectsecondoption =false;
+  	this.selectfirstoption =false;
   }
 
 }
--- src/app/component/thumbnailsphoto/thumbnailsphoto.service.ts
+++ src/app/component/thumbnailsphoto/thumbnailsphoto.service.ts
@@ -1,8 +1,18 @@
 import { Injectable } from '@angular/core';
+import { Http, Response, Headers, RequestOptions} from '@angular/http';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/observable/throw';
+import 'rxjs/add/operator/map';
 
 @Injectable()
 export class ThumbnailsphotoService {
 
-  constructor() { }
 
+  constructor(private _http : Http ) { }
+
+  public getviewselection(){
+		let _url: string="https://api.myjson.com/bins/m17wt";
+		return this._http.get(_url)
+		.map((response: Response) => {return response.json();});
+	}
 }
