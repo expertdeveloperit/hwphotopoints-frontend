7474e33284d573156efd5ff73bc4a143c4e80fe2
Expert IT <expert.developer.it@gmail.com>
2017-09-14 19:53:41 +0530

    script changes

--- src/app/component/imagedetails/imagedetails.component.ts
+++ src/app/component/imagedetails/imagedetails.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit,Input} from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
 import {ImageDetailService} from './imagedetails.service';
+import 'rxjs/Rx' ;
 
 @Component({
   selector: 'app-imagedetails',
@@ -44,6 +45,9 @@ export class ImagedetailsComponent implements OnInit {
       }
     });
   }
+
+  // ---=== Delete image ---===
+  
   deleteImage(){
      if(this.imageId != 0){
       if(confirm("Are you sure you want to delete this?")){
@@ -62,10 +66,22 @@ export class ImagedetailsComponent implements OnInit {
       
     }
   }
+
+  // ---=== Dawnload Image ---===
+  download(downloadLink){
+       var blob = new Blob([downloadLink], { type: 'text/csv' });
+          var imageUrl= window.URL.createObjectURL(blob);
+        window.open(imageUrl);
+  }
+
+  // ---=== Show Image data ---=== 
+
   showExifdata(){
   	this.exifData = true;
+    this.fileData = false;
   }
   showFiledata(){
   	this.fileData = true;
+    this.exifData = false;
   }
 }
--- src/app/component/shared/header/header.component.ts
+++ src/app/component/shared/header/header.component.ts
@@ -2,16 +2,34 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router'; //--- ==== import for routing
 import {CookieService} from 'angular2-cookie/core';
 import {SharedDataService} from '../../../shared-data.service';
+// ---===import for animation---===
+import { animate, style, state, transition, trigger} from '@angular/core';
+
 @Component({
   selector: 'app-new-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
+    animations: [
+    trigger("fadeInOut", [
+      state("open", style({opacity: 1})),
+      state("closed", style({opacity: 0})),
+      transition("open <=> closed", animate( "3000ms" )),
+    ])
+    ]
 })
 export class NewHeaderComponent implements OnInit {
-userAuthenticated:boolean;
+
+// ---=== These are animation states---===
+  state = 'open';
+  timeOutRef;
+
+// ---=== boolean value defined ---===
+  userAuthenticated:boolean;
+  menuiconshow:boolean;
 
   constructor(private router: Router, private _cookieService:CookieService,public serve : SharedDataService) { 
   	this.userAuthenticated=false;
+    this.menuiconshow=false;
   	let loggedinUser = this._cookieService.get("hwUserToken");
    		if(loggedinUser){
    			this.userAuthenticated=true;
@@ -20,10 +38,20 @@ userAuthenticated:boolean;
 
   ngOnInit() {
   }
+  
   logoutUser(){
     this.userAuthenticated = false;
 		this._cookieService.remove("hwUserToken",'/');
 		this._cookieService.remove("userDetail",'/');
 		this.router.navigate(['/login']);
 	}
+  toggleClass(){
+    this.menuiconshow=true;
+    console.log("djghkdf");
+  }
+  toggleli(){
+    this.menuiconshow=false;
+  }
+
+
 }
