072741a690f99b84aa81ef8b7cc40989f29ccf73
Expert IT <expert.developer.it@gmail.com>
2017-09-16 10:57:28 +0530

    changes

--- src/app/component/shared/header/header.component.ts
+++ src/app/component/shared/header/header.component.ts
@@ -18,8 +18,6 @@ import { animate, style, state, transition, trigger} from '@angular/core';
     ]
 })
 export class NewHeaderComponent implements OnInit {
-
-<<<<<<< HEAD
 // ---=== These are animation states---===
   state = 'open';
   timeOutRef;
@@ -28,51 +26,26 @@ export class NewHeaderComponent implements OnInit {
   userAuthenticated:boolean;
   // menuiconshow:boolean;
 
-  constructor(private router: Router, private _cookieService:CookieService,public serve : SharedDataService) { 
-    this.userAuthenticated=false;
-    // this.menuiconshow=false;
-    let loggedinUser = this._cookieService.get("hwUserToken");
-       if(loggedinUser){
-         this.userAuthenticated=true;
-       }
-=======
-
+  
   constructor(private router: Router, private _cookieService:CookieService,public serve : SharedDataService) { 
   	let loggedinUser = this._cookieService.get("hwUserToken");
    		if(loggedinUser){
    			this.serve.hitLogin(false);
    		}else this.serve.hitLogin(true);
->>>>>>> 37486a66ef8d4e7f3c45169775b6ea087344e17f
+
   }
 
   ngOnInit() {
 
   }
-<<<<<<< HEAD
+
   
-  logoutUser(){
-    this.userAuthenticated = false;
-    this._cookieService.remove("hwUserToken",'/');
-    this._cookieService.remove("userDetail",'/');
-    this.router.navigate(['/login']);
-  }
-  // toggleClass(){
-  //   this.menuiconshow=true;
-  //   console.log("djghkdf");
-  // }
-  // toggleli(){
-  //   this.menuiconshow=false;
-  // }
-
-
-=======
-
-
+ 
   logoutUser(){
     this.serve.hitLogin(true);
     this._cookieService.remove("hwUserToken",'/');
 		this._cookieService.remove("userDetail",'/');
 		this.router.navigate(['/login']);
 	}
->>>>>>> 37486a66ef8d4e7f3c45169775b6ea087344e17f
+
 }
